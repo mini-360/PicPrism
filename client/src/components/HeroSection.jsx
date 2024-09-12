764b065ab74c7d770f052b0bdb774147acf6044b
Aniket Singh <aniket.singh07vs@gmail.com>
2024-09-13 02:13:20 +0530

    main section fully completed

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -3,13 +3,13 @@ import { IoIosSearch } from "react-icons/io";
 
 const HeroSection = () => {
   return (
-    <div className="sm:w-[80vw] h-[20vh] overflow-clip sm:rounded-3xl mx-auto flex justify-center items-center">
+    <div className="md:w-[80vw] h-[20vh] overflow-clip sm:rounded-3xl mx-auto flex justify-center items-center">
       <form action="" className=" flex items-center justify-center">
         <input
           type="search"
           id="search"
           name="search"
-          className="py-5 px-3 w-[70vw] text-xl sm:text-3xl mx-auto outline-none border-b-2 "
+          className="py-5 px-3 md:w-[70vw] text-xl sm:text-3xl mx-auto outline-none border-b-2 bg-bgColor"
           placeholder="Search your assest...."
         />
         <IoIosSearch className="text-3xl sm:text-5xl text-gray-500 -ml-12" />
