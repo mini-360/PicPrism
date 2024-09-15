c9e74ead946ce1067ae306892f42d868db020f52
mini-360 <rajabhay56999@gmail.com>
2024-09-16 01:57:51 +0530

    router and controller setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
-import  "dotenv/config";
+import "dotenv/config";
+
+import authRoute from "./routes/authRoute.js";
+import  {connectDb}  from "./connection.js";
 
 const app = express();
+
+connectDb()
 
 app.get("/", (req, res) => {
   res.send("<center><h1>Server running</h1></center>");
 });
+
+app.use("/api", authRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
