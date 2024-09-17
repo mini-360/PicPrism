a05e66504a87975624cbfd7505d3e0d9c61302d8
mini-360 <rajabhay56999@gmail.com>
2024-09-17 17:38:31 +0530

    signup route created

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,32 @@
-const login = async (req, res) => {};
+import { User } from "../models/user.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const signup = async (req, res) => {};
+const signup = async (req, res) => {
+  const { username, email, password, accountType } = req.body;
+
+  try {
+    let user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already in use" });
+    }
+    const securePassword = await bcrypt.hash(password, 10);
+    user = new User({
+      username,
+      email,
+      password: securePassword,
+      accountType,
+    });
+      await user.save()
+
+      return res.status(201).json({success:true,message:"User created successfully"})
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const login = async (req, res) => {};
 
 export { login, signup };
