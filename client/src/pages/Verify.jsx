d59db6a7f3c08b4ffa1a409150ffabe5489159a6
Aniket Singh <aniket.singh07vs@gmail.com>
2024-09-18 15:58:13 +0530

    Authentication css completed

--- client/src/pages/Verify.jsx
+++ client/src/pages/Verify.jsx
@@ -22,32 +22,54 @@ const Verify = () => {
   return (
     <>
       <Toaster />
-      <div>
-        <form action="" onSubmit={handleVerify}>
-          <h1>otp verification page</h1>
-          <div>
-            <label htmlFor="otp">Otp</label>
+      <div className="flex items-center justify-center w-full min-h-screen">
+        <form
+          action=""
+          onSubmit={handleVerify}
+          className="bg-white shadow-md rounded-md px-10 py-6 w-full sm:w-[30vw]"
+        >
+          <h1 className="text-center font-bold text-2xl mb-7">
+            OTP verification
+          </h1>
+          <div className="mb-4">
+            <label htmlFor="otp" className="block font-bold text-gray-700 mb-2">
+              Otp :
+            </label>
             <input
               type="otp"
               name="otp"
               id="otp"
               value={otp}
               onChange={(e) => setOtp(e.target.value)}
+              placeholder="Enter your otp"
+              className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:border-black focus:ring-black "
             />
           </div>
-          <div>
-            <label htmlFor="">New Password</label>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block font-bold text-gray-700 mb-2"
+            >
+              New Password :
+            </label>
             <input
               type="password"
               name="password"
               id="password"
+              placeholder="Enter your new password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              className="shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:border-black focus:ring-black "
             />
           </div>
 
-          <div>
-            <button type="submit">verify otp</button>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="bg-black text-white shadow-md rounded-md w-full px-3 py-2 border-gray-300 focus:outline-none focus:border-black focus:ring-black"
+            >
+              Verify
+            </button>
           </div>
         </form>
       </div>
